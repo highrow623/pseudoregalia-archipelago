ce2e2deaa067d91f858b7fc1e29419cbf615eea3
yoyowazzap <danieltmetro@gmail.com>
2025-03-15 16:55:13 -0600

    refactored tricks classes to handle reading json made some variables in rules.__init__ part of self so they can be inspected after creation

--- AP_Randomizer/apworld/rules.py
+++ AP_Randomizer/apworld/rules.py
@@ -3,7 +3,7 @@ import json
 from BaseClasses import CollectionState
 from typing import Dict, Callable, List, Set, TYPE_CHECKING
 from worlds.generic.Rules import set_rule
-from tricks import LogicTricks, Trick, Loadout
+from .tricks import LogicTricks, Trick, json_to_logic_tricks, state_to_loadout, loadout_to_bit_rep, add_to_minimal_set
 from .constants.tags import ONLY_REQUIRE_SIX_KEYS
 from .constants.names import LOCATION_SUN_GREAVES
 
@@ -20,30 +20,35 @@ class PseudoregaliaRules:
     location_rules: Dict[str, Callable[[CollectionState], bool]]
     required_small_keys: int = 7 # Set to 6 for only_require_6_keys tag
 
+    logic_tricks: LogicTricks
+    player_tags: Set[str]
+    entrance_trick_bit_reps: Dict[str, Set[int]]
+    location_trick_bit_reps: Dict[str, Set[int]]
+
     def __init__(self, world: PseudoregaliaWorld) -> None:
         self.world = world
         self.player = world.player
 
-        logic_tricks = self.load_logic_tricks()
-        tags = self.get_tags(logic_tricks)
+        self.logic_tricks = self.load_logic_tricks()
+        self.player_tags = self.get_tags()
 
-        if ONLY_REQUIRE_SIX_KEYS in tags:
+        if ONLY_REQUIRE_SIX_KEYS in self.player_tags:
             self.required_small_keys = 6
 
-        entrance_trick_bit_reps = self.filter_tricks(logic_tricks.entrance_tricks, tags)
-        location_trick_bit_reps = self.filter_tricks(logic_tricks.location_tricks, tags)
+        self.entrance_trick_bit_reps = self.filter_tricks(self.logic_tricks.entrance_tricks)
+        self.location_trick_bit_reps = self.filter_tricks(self.logic_tricks.location_tricks)
 
-        self.entrance_rules = self.build_rules(entrance_trick_bit_reps)
-        self.location_rules = self.build_rules(location_trick_bit_reps)
+        self.entrance_rules = self.build_rules(self.entrance_trick_bit_reps)
+        self.location_rules = self.build_rules(self.location_trick_bit_reps)
 
     def load_logic_tricks(self) -> LogicTricks:
         current_dir = os.path.dirname(os.path.realpath(__file__))
         tricks_path = os.path.join(current_dir, "tricks.json")
         with open(tricks_path) as f:
-            logic_tricks: LogicTricks = json.load(f)
-            return logic_tricks
+            logic_tricks_dict = json.load(f)
+            return json_to_logic_tricks(logic_tricks_dict)
 
-    def get_tags(self, tag_hierarchy: Dict[str, List[str]]) -> Set[str]:
+    def get_tags(self) -> Set[str]:
         tags: Set[str] = set() # already evaluated
         tag_queue: Set[str] = set() # to add but not yet evaluated
         for tag in self.world.options.trick_tags.value:
@@ -56,27 +61,28 @@ class PseudoregaliaRules:
             tag = tag_queue.pop()
             tags.add(tag)
 
-            if tag not in tag_hierarchy:
+            if tag not in self.logic_tricks.tag_hierarchy:
                 continue
 
-            for child_tag in tag_hierarchy[tag]:
+            for child_tag in self.logic_tricks.tag_hierarchy[tag]:
                 if child_tag in tags:
                     continue
                 tag_queue.add(child_tag)
     
-    def filter_tricks(self, rules: Dict[str, List[Trick]], player_tags: Set[str]) -> Dict[str, Set[int]]:
+    def filter_tricks(self, rules: Dict[str, List[Trick]]) -> Dict[str, Set[int]]:
         trick_bit_reps: Dict[str, Set[int]] = {}
         for name, tricks in rules.items():
-            trick_bit_reps[name] = []
+            trick_bit_reps[name] = set()
             for trick in tricks:
                 is_default_trick = len(trick.tags) == 0
                 is_included = trick.id in self.world.options.include_trick_ids.value
                 is_excluded = trick.id in self.world.options.exclude_trick_ids.value
-                player_has_tags = set(trick.tags) <= player_tags
+                player_has_tags = trick.tags <= self.player_tags
                 if is_default_trick or is_included or not is_excluded and player_has_tags:
-                    trick_bit_reps[name].add(trick.loadout.to_bit_rep())
+                    trick_bit_rep = loadout_to_bit_rep(trick.loadout)
+                    trick_bit_reps[name] = add_to_minimal_set(trick_bit_reps[name], trick_bit_rep)
         return trick_bit_reps
-    
+
     def build_rules(self, rules_bit_reps: Dict[str, Set[int]]) -> Dict[str, Callable[[CollectionState], bool]]:
         rules: Dict[str, Callable[[CollectionState], bool]] = {}
         for name, trick_bit_reps in rules_bit_reps.items():
@@ -88,7 +94,8 @@ class PseudoregaliaRules:
             if len(trick_bit_reps) == 0:
                 return True
 
-            state_bit_rep = Loadout(state, self.player, self.required_small_keys).to_bit_rep()
+            state_loadout = state_to_loadout(state, self.player, self.required_small_keys)
+            state_bit_rep = loadout_to_bit_rep(state_loadout)
             for trick_bit_rep in trick_bit_reps:
                 if trick_bit_rep & state_bit_rep == trick_bit_rep:
                     return True
@@ -106,8 +113,7 @@ class PseudoregaliaRules:
         for name, rule in self.location_rules.items():
             if name == LOCATION_SUN_GREAVES and split_kicks:
                 for i in range(1, 4):
-                    new_name = f"{name} {i}"
-                    location = multiworld.get_location(new_name, self.player)
+                    location = multiworld.get_location(f"{name} {i}", self.player)
                     set_rule(location, rule)
                 continue
             location = multiworld.get_location(name, self.player)
--- AP_Randomizer/apworld/tricks.py
+++ AP_Randomizer/apworld/tricks.py
@@ -1,6 +1,8 @@
-from typing import Dict, List, Set
+from dataclasses import dataclass
+from typing import Dict, Set, List, Any
 from BaseClasses import CollectionState
 
+@dataclass
 class Loadout:
     dream_breaker: bool = False
     strikebreak: bool = False
@@ -13,50 +15,93 @@ class Loadout:
     kicks: int = 0
     small_keys: bool = False
 
-    def __init__(self, state: CollectionState, player: int, required_small_keys: int):
-        self.dream_breaker = state.has_any({"Dream Breaker", "Progressive Dream Breaker"}, player)
-        self.strikebreak = state.has("Strikebreak", player) or state.count("Progressive Dream Breaker", player) >= 2
-        self.soul_cutter = state.has("Soul Cutter", player) or state.count("Progressive Dream Breaker", player) >= 3
-        self.sunsetter = state.has("Sunsetter", player)
-        self.slide = state.has_any({"Slide", "Progressive Slide"}, player)
-        self.solar_wind = state.has("Solar Wind", player) or state.count("Progressive Slide", player) >= 2
-        self.ascendant_light = state.has("Ascendant Light", player)
-        self.cling_gem = state.has("Cling Gem", player)
-        self.kicks += 3 if state.has("Sun Greaves", player) else 0
-        self.kicks += state.count("Heliacal Power", player)
-        self.kicks += state.count("Air Kick", player)
-        self.small_keys = state.count("Small Key") >= required_small_keys
-    
-    def to_bit_rep(self) -> int:
-        bit_rep = 0
-        mask = 1
-
-        def mark_if_true(condition: bool):
-            nonlocal bit_rep, mask
-            if condition:
-                bit_rep = bit_rep | mask
-            mask = mask << 1
-
-        mark_if_true(self.dream_breaker)
-        mark_if_true(self.strikebreak)
-        mark_if_true(self.soul_cutter)
-        mark_if_true(self.sunsetter)
-        mark_if_true(self.slide)
-        mark_if_true(self.solar_wind)
-        mark_if_true(self.ascendant_light)
-        mark_if_true(self.cling_gem)
-        for i in range(1, 5):
-            mark_if_true(self.kicks >= i)
-        mark_if_true(self.small_keys)
-
-        return bit_rep
-
+@dataclass
 class Trick:
     id: str
     loadout: Loadout
-    tags: List[str]
+    tags: Set[str]
 
+@dataclass
 class LogicTricks:
     entrance_tricks: Dict[str, List[Trick]]
     location_tricks: Dict[str, List[Trick]]
-    tag_hierarchy: Dict[str, List[str]]
+    tag_hierarchy: Dict[str, Set[str]]
+
+def json_to_trick(trick_json) -> Trick:
+    # the trick_json comes from tricks.json
+    # it should have the same structure as Trick, which is assumed here
+    return Trick(
+        id = trick_json["id"],
+        loadout = Loadout(**(trick_json["loadout"])),
+        tags = set(trick_json["tags"]) if "tags" in trick_json else set(),
+    )
+
+def json_to_trick_list_dict(trick_list_dict_json) -> Dict[str, List[Trick]]:
+    return {name: [json_to_trick(trick_json) for trick_json in trick_list_json] for name, trick_list_json in trick_list_dict_json.items()}
+
+def json_to_str_set_dict(str_list_dict_json) -> Dict[str, Set[str]]:
+    return {name: set(json_list) for name, json_list in str_list_dict_json.items()}
+
+def json_to_logic_tricks(logic_tricks_json) -> LogicTricks:
+    # the logic_tricks_json comes from tricks.json
+    # it should have the same structure as LogicTricks, which is assumed here
+    return LogicTricks(
+        entrance_tricks=json_to_trick_list_dict(logic_tricks_json["entrance_tricks"]),
+        location_tricks=json_to_trick_list_dict(logic_tricks_json["location_tricks"]),
+        tag_hierarchy=json_to_str_set_dict(logic_tricks_json["tag_hierarchy"]))
+
+def state_to_loadout(state: CollectionState, player: int, required_small_keys: int) -> Loadout:
+    kicks = 3 if state.has("Sun Greaves", player) else 0
+    kicks += state.count("Heliacal Power", player)
+    kicks += state.count("Air Kick", player)
+
+    return Loadout(
+        dream_breaker = state.has_any({"Dream Breaker", "Progressive Dream Breaker"}, player),
+        strikebreak = state.has("Strikebreak", player) or state.count("Progressive Dream Breaker", player) >= 2,
+        soul_cutter = state.has("Soul Cutter", player) or state.count("Progressive Dream Breaker", player) >= 3,
+        sunsetter = state.has("Sunsetter", player),
+        slide = state.has_any({"Slide", "Progressive Slide"}, player),
+        solar_wind = state.has("Solar Wind", player) or state.count("Progressive Slide", player) >= 2,
+        ascendant_light = state.has("Ascendant Light", player),
+        cling_gem = state.has("Cling Gem", player),
+        kicks = kicks,
+        small_keys = state.count("Small Key") >= required_small_keys,
+    )
+
+def loadout_to_bit_rep(loadout: Loadout) -> int:
+    bit_rep = 0
+    mask = 1
+
+    def mark_if_true(condition: bool):
+        nonlocal bit_rep, mask
+        if condition:
+            bit_rep = bit_rep | mask
+        mask = mask << 1
+
+    mark_if_true(loadout.dream_breaker)
+    mark_if_true(loadout.strikebreak)
+    mark_if_true(loadout.soul_cutter)
+    mark_if_true(loadout.sunsetter)
+    mark_if_true(loadout.slide)
+    mark_if_true(loadout.solar_wind)
+    mark_if_true(loadout.ascendant_light)
+    mark_if_true(loadout.cling_gem)
+    for i in range(1, 5):
+        mark_if_true(loadout.kicks >= i)
+    mark_if_true(loadout.small_keys)
+
+    return bit_rep
+
+def add_to_minimal_set(bit_reps: Set[int], bit_rep: int) -> Set[int]:
+    new_minimal_set: Set[int] = {bit_rep}
+    for known_bit_rep in bit_reps:
+        if known_bit_rep & bit_rep == known_bit_rep:
+            # the new bit_rep is covered by a more restrictive known_bit_rep
+            return bit_reps
+        if known_bit_rep & bit_rep == bit_rep:
+            # the new bit_rep is more restrictive than the known_bit_rep
+            # so we can get rid of the known one
+            continue
+        # otherwise, the bit_reps are incomparable, so we keep both
+        new_minimal_set.add(known_bit_rep)
+    return new_minimal_set
